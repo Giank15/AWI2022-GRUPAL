1d2a148deed619137b314b3a846c5325ae95e9ee
Giank15 <giancarlo_queirolo.15@outlook.es>
2022-05-18 22:08:10 -0500

    Primeras paginas del proyecto

---
+++ refugioAnimales/main.js
@@ -0,0 +1,29 @@
+import './style.css'
+
+let html = ``;
+
+html+=`<header>`;
+html+=`<h1>GIGI Animal Shelter</h1>`;
+html+=`<h2>Rescate y adopción de animalitos :3</h2>`;
+html+=`</header>`;
+
+html+=`<article>`;
+html+=`</article>`;
+
+html+=`<aside>`;
+html+=`</aside>`;
+
+html+=`<nav>`;
+html+=`<ul>`;
+html+=`<li>`;
+html+=`<a href="./pages/Informacion/infoProyecto.html">Información del proyecto</a>`;
+html+=`</li>`;
+html+=`<li>`;
+html+=`</li>`;
+html+=`<li>`;
+html+=`</li>`;
+html+=`</ul>`;
+html+=`</nav>`;
+
+
+app.innerHTML = html;
