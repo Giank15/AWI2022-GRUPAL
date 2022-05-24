f10607f9b189875f7ca7684e52781c89c4c56b7e
Giank15 <giancarlo_queirolo.15@outlook.es>
2022-05-24 11:33:08 -0500

    Pagina acerca de e informacion del proyecto terminadas

---
+++ refugioAnimales/pages/Acerca/acerca.js
@@ -0,0 +1,108 @@
+import './estilos1.css'
+import './estilos2.css'
+
+let html = ``;
+
+html+=`<header class="header">`;
+//Log de Header
+html+=`<div class="logo">`;
+html+=`<div class="imagen-logo">`;
+html+=`<img src="../../images/logoperrito.png" alt="">`; 
+html+=`</div>`;
+html+=`<div class="contenedor-letras-logo">`;
+html+=`<h1>Gigi Animal Shelter</h1>`;
+html+=`<p>Rescate y adopción de animalitos</p>`;
+html+=`</div>`;
+html+=`<i class="list material-icons bar-active" onclick="barActive()">list</i>`;
+html+=`</div>`;
+//Navegacion
+html+=`<nav class="nadvar" id="nadvar">`;
+html+=`<ul>`;
+let paginas = [
+    {url: "../index.html", descripcion:"Pagina Principal" },
+    {url: "Administracion/administracion.html", descripcion:"Administracion" },
+    {url: "Informacion/infoProyecto.html", descripcion:"Informacion" }
+]
+for (let elemento of paginas)
+{
+    html+=`<li>`;
+    html+=`<a href="../${elemento.url}">${elemento.descripcion}</a>`;
+    html+=`</li>`;
+}
+html+=`</ul>`;
+html+=`</nav>`;
+html+=`</header>`;
+
+html+=`<main class="main container">`;
+html+=`<section class="seccion-refugio">`;
+html+=`<div class="perfil-refugio">`;
+html+=`<h2>Acerca de proyecto</h2>`;
+html+=`<hr>`;
+html+=`</div>`;
+html+=`<div">`;
+html+=`<section id="hero">`;
+html+=`<h1><br>Giusepy Alexander Calle Santana Giancarlo <br>Stephano Queirolo Rodriguez`;
+html+=`<br>Ligia Nicole Vargas Garcia</h1>`;
+html+=`<button>Saber Mas</button>`;
+html+=`</section>`;
+html+=`<section id="Somos-proya">`;
+html+=`<div class="container">`;
+html+=`<div class="img container"></div>`;
+html+=`<div class="texto">`;
+html+=`<h2><spam class="color-acento">Conoce acerca de</spam></h2>`;
+html+=`<p> Nuestra organización protectora de animales especializada,en la ayuda a perros y gatos maltratados y/o abandonados.
+Una organización totalmente independiente que no recibe subvenciones de organismos oficiales, 
+empresas ni partidos políticos.Las cuotas de nuestros socios y las donaciones de nuestros simpatizantes 
+permiten que nuestra labor sea sostenible y preservan nuestra libertad de acción.</p>`;
+html+=`</div>`;
+html+=`</div>`;
+     
+html+=`</section>`;
+html+=`<section id="nuestros-programas">`;
+html+=`<div class="container">`;
+html+=`<h2>Adopciones de</h2>`;
+html+=`<div class="progrmas">`;
+html+=`<div class="carta">`;
+html+=`<h3>Perrros</h3>`;
+html+=`<p>Pasar tiempo con tu mascota puede resultar muy terapéutico, 
+especialmente si estás atravesando un momento emocionalmente difícil.</p>`;
+html+=`<button>+info</button>`;
+html+=`</div>`;
+html+=`<div class="carta">`;
+html+=`<h3>Gatos</h3>`;
+html+=`<p>Al adoptar, no solo iluminas la vida de un animal, además ayudas a liberar espacio en un albergue, 
+lo que permite rescatar a más animales abandonados a su suerte. 6.</p>`;
+html+=`<button>+info</button>`;
+html+=`</div>`;
+html+=`<div class="carta">`;
+html+=`<h3>Otros</h3>`;
+html+=`<p>En la mayoría de los casos, adoptar significa darle una
+segunda oportunidad a un animal que ha sufrido un proceso de abandono, 
+y en ocasiones maltrato.</p>`;
+html+=`<button>+info</button>`;
+html+=`</div>`;
+html+=`</div>`;
+html+=`</div>`;
+html+=`</section>`;
+
+html+=`<section id="caracteristica">`;
+html+=`<div class="container">`;
+html+=`<ul>`;
+html+=`<li>✅Vacunas</li>`;
+html+=`<li>✅Chequeos</li>`;
+html+=`<li>✅Atencion veterinaria</li>`;
+html+=`<li>✅Esterilización de machos y hembras</li>`;
+html+=`</ul>`;
+html+=`</div>`;
+html+=`</section>`;
+
+html+=`</div>`;
+html+=`</section>`;
+html+=`</main>`;
+
+html+=`<footer class="footer">`;
+html+=`<p>Aplicaciones Web 1</p>`;
+html+=`<p>Designed with by <a href="#">Estudiantes de 5to A</a></p>`;
+html+=`</footer>`;
+
+app.innerHTML = html;
---
+++ refugioAnimales/pages/Informacion/infoProyecto.js
@@ -0,0 +1,90 @@
+import './estilos.css'
+
+let html = ``;
+
+html+=`<header class="header">`;
+//Log de Header
+html+=`<div class="logo">`;
+html+=`<div class="imagen-logo">`;
+html+=`<img src="../../images/logoperrito.png" alt="">`; 
+html+=`</div>`;
+html+=`<div class="contenedor-letras-logo">`;
+html+=`<h1>Gigi Animal Shelter</h1>`;
+html+=`<p>Rescate y adopción de animalitos</p>`;
+html+=`</div>`;
+html+=`<i class="list material-icons bar-active" onclick="barActive()">list</i>`;
+html+=`</div>`;
+//Navegacion
+html+=`<nav class="nadvar" id="nadvar">`;
+html+=`<ul>`;
+let paginas = [
+    {url: "Acceso/accesoUsuario.html", descripcion:"Acceder" },
+    {url: "Administracion/administracion.html", descripcion:"Administracion" },
+    {url: "Registro/registroUsuario.html", descripcion:"Registrase" },
+    {url: "Informacion/infoProyecto.html", descripcion:"Informacion" },
+    {url: "Acerca/acerca.html", descripcion:"Acerca de" }
+]
+for (let elemento of paginas)
+{
+    html+=`<li>`;
+    html+=`<a href="../${elemento.url}">${elemento.descripcion}</a>`;
+    html+=`</li>`;
+}
+html+=`</ul>`;
+html+=`</nav>`;
+html+=`</header>`;
+
+html+=`<main class="main container">`;
+html+=`<section class="seccion-refugio">`;
+html+=`<div class="perfil-refugio">`;
+html+=`<h2>Informacion del Refugio</h2>`;
+html+=`<hr>`;
+html+=`</div>`;
+html+=`<div class="informacion-refugio">`;
+html+=`<section>`;
+html+=`<h1>Nombre del Proyecto: GiGi Animal Shelter</h1>`;
+html+=`<h2>Tematica: Regugio de rescate y adopción de animalitos</h2>`;
+html+=`</section>`;
+
+html+=`<section>`;
+html+=`<p>
+El tema de nuestro proyecto consiste en un aplicativo web de un refugio de animales en la que las personas podrán donar para aquellos animales que se encuentran
+en un mal estado y necesitan para su tratamiento. Además tendrán la opción de poder adoptar a los animales desde el aplicativo.`;
+html+=`</p>`;
+html+=`</section>`;
+  
+html+=`<section>`;
+html+=`<h2>Contenido multimedia de nuestro refugio GiGi</h2>`;
+html+=`<figure>`;
+html+=`<img src="../../images/ref1.jpg" alt="refugio de perrito" width="350px" height="200px">`;
+html+=`<img src="../../images/red2.jpg" alt="refugio de perrito" width="350px" height="200px">`;
+html+=`<img src="../../images/ref3.jpg" alt="refugio de perrito" width="350px" height="200px">`;
+html+=`</figure>`;
+html+=`</section>`;
+
+html+=`<section>`;
+html+=`<p>Caracteristicas del proyecto a continuación</p>`;
+html+=`<table border=1 class="tablai">`;
+html+=`<tr>`;
+html+=`<td>Característica</td>`;
+html+=`<td>Recursos</td>`;
+html+=`<td>Tiempo</td>`;
+html+=`</tr>`;
+html+=`<tr>`;
+html+=`<td>pppp</td>`;
+html+=`<td>jjjj</td>`;
+html+=`<td>hhh</td>`;
+html+=`</tr>`;
+html+=`</table>`;
+html+=`</section>`;
+
+html+=`</div>`;
+html+=`</section>`;
+html+=`</main>`;
+
+html+=`<footer class="footer">`;
+html+=`<p>Aplicaciones Web 1</p>`;
+html+=`<p>Designed with by <a href="#">Estudiantes de 5to A</a></p>`;
+html+=`</footer>`;
+
+app.innerHTML = html;
