ae6b2ed7feb227e0cb3ce6d6952235352ea68b64
GiusepyCalle02 <e1315207892@live.uleam.edu.ec>
2022-07-21 10:40:25 -0500

    Página de Adopción Finalizada

--- refugio2/src/adopcion.jsx
+++ refugio2/src/adopcion.jsx
@@ -5,7 +5,50 @@ let html = ``;
 
 function Adopcion (){
 
+    useEffect(()=> {
+        let html1 = ''
+        let animales = [
+            {nombre: "TOBBY", raza:"JACK RUSSELL TERRIER", codigo: "C001", imagen: "../../images/adop1.jpg" },
+            {nombre: "LOCKI", raza:"MIXTA", codigo: "C002", imagen: "../../images/adop2.jpg"},
+            {nombre: "PANCHO", raza:"BLACK MOUTH CUR", codigo: "C003", imagen: "../../images/adop3.jpg" },
+            {nombre: "LOLO", raza:"LABRADOR RETRIEVER", codigo: "C004", imagen: "../../images/adop4.jpg" },
+            {nombre: "LUKAS", raza:"WEST HIGHLAND", codigo: "C005", imagen: "../../images/adop5.jpg" },
+            {nombre: "REIKO", raza:"SPANIEL BRETÓN", codigo: "C006", imagen: "../../images/adop6.jpeg" },
+            {nombre: "COQUI", raza:"CANICHE O POODLE TOY", codigo: "C007", imagen: "../../images/adop7.jpg" },
+            {nombre: "PIPO", raza:"MIXTA", codigo: "C008", imagen: "../../images/adop8.jpg" },
+            {nombre: "CORTIS", raza:"MIXTA", codigo: "C009", imagen: "../../images/adop9.jfif" },
+            {nombre: "RAYO", raza:"BLACK MOUTH CUR", codigo: "C010", imagen: "../../images/adop10.jfif" }
+        ]
+        for (let elemento of animales)
+        {
+        html+=`<div class="sub-grid">`
+        html+=`<p class="descripcion"> <b>Nombre:</b> ${elemento.nombre}</p>`
+        html+=`<p class="descripcion"> <b>Raza:</b> ${elemento.raza} </p>`
+        html+=`<p class="descripcion"> <b>Código:</b> ${elemento.codigo}</p>`
+        html+=`<img src=${elemento.imagen} alt=""> </img>`
+        html+=`<button class="send-button"><a href="https://www.dkidscayambe.com/wp-content/uploads/2018/07/CF7-ADOPCION.pdf" class="adoptar-palabra">Adoptar</a></button>`
+        html+=`</div>`
+        }
+        grid_refugio.innerHTML=html1;
+    })
+
+    useEffect(()=> {
+        let html = ''
+        let paginas = [
+          {url: "accesoUsuario.jsx", descripcion:"Acceder" },
+          {url: "Registro/registroUsuario.html", descripcion:"Registrase" },
+          {url: "Informacion/infoProyecto.html", descripcion:"Informacion" },
+          {url: "Adopcion/adopcion.html", descripcion:"Adopcion" },
+          {url: "Acerca/acerca.html", descripcion:"Acerca de" }
+        ]
     
+        for (let elemento of paginas){
+          html+=`<li>`;
+          html+=`<a href="./${elemento.url}">${elemento.descripcion}</a>`;
+          html+=`</li>`;
+        }
+        opMenu.innerHTML = html;
+      });
 
     return(
         <div className='adopcion'>
